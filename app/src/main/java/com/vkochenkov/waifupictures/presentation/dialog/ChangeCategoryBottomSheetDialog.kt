5348a015be5dced9459182bb334419b966168e2e
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-08-23 17:25:51 +0300

    complete bottom sheet category

--- app/src/main/java/com/vkochenkov/waifupictures/presentation/dialog/ChangeCategoryBottomSheetDialog.kt
+++ app/src/main/java/com/vkochenkov/waifupictures/presentation/dialog/ChangeCategoryBottomSheetDialog.kt
@@ -1,14 +1,17 @@
 package com.vkochenkov.waifupictures.presentation.dialog
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.vkochenkov.waifupictures.R
+import com.vkochenkov.waifupictures.data.api.NetworkStorage
 import com.vkochenkov.waifupictures.data.model.Category
 import com.vkochenkov.waifupictures.di.App
 import com.vkochenkov.waifupictures.presentation.adapter.ItemClickListener
@@ -18,7 +21,7 @@ import com.vkochenkov.waifupictures.presentation.view_model.PicturesViewModel
 import com.vkochenkov.waifupictures.presentation.view_model.ViewModelFactory
 import javax.inject.Inject
 
-class ChangeCategoryBottomSheetDialog: BottomSheetDialogFragment() {
+class ChangeCategoryBottomSheetDialog : BottomSheetDialogFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -26,6 +29,8 @@ class ChangeCategoryBottomSheetDialog: BottomSheetDialogFragment() {
     private val picturesViewModel: PicturesViewModel by lazy {
         ViewModelProvider(requireActivity(), viewModelFactory).get(PicturesViewModel::class.java)
     }
+
+    lateinit var currentCategoryTv: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +43,8 @@ class ChangeCategoryBottomSheetDialog: BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.bottom_sheet_category, container, false)
+        currentCategoryTv = root.findViewById(R.id.category_current_tv)
+        currentCategoryTv.text = makeCategoryText(NetworkStorage.lastChangedCategory)
         initRecyclerView(root)
         return root
     }
@@ -45,11 +52,17 @@ class ChangeCategoryBottomSheetDialog: BottomSheetDialogFragment() {
     private fun initRecyclerView(root: View) {
         val categoryRecycler = root.findViewById<RecyclerView>(R.id.categories_recycler)
         categoryRecycler.layoutManager = LinearLayoutManager(root.context)
-        categoryRecycler.adapter = CategoryAdapter(object : ItemClickListener<CategoryViewHolder, Category> {
-            override fun onItemCLick(holder: CategoryViewHolder, item: Category) {
-                picturesViewModel.onCategorySelected(item)
-                this@ChangeCategoryBottomSheetDialog.dismiss()
-            }
-        })
+        categoryRecycler.adapter =
+            CategoryAdapter(object : ItemClickListener<CategoryViewHolder, Category> {
+                override fun onItemCLick(holder: CategoryViewHolder, item: Category) {
+                    picturesViewModel.onCategorySelected(item)
+                    currentCategoryTv.text = makeCategoryText(item)
+                    this@ChangeCategoryBottomSheetDialog.dismiss()
+                }
+            })
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun makeCategoryText(category: Category) =
+        "${requireActivity().getText(R.string.category_label_str)} ${category.text}"
 }
